3372e0064f2cc3205fa6cefbc77261f77fbdf82d
Gopichand173 <gopi110531@gmail.com>
2026-03-04 12:08:41 -0600

    roboshop code

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,15 @@
+# 1. Setup Erlang and RabbitMQ Repos
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+# 2. Install RabbitMQ
 dnf install rabbitmq-server -y
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+
+# 3. Start and Enable the service
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+
+# 4. Create the User (Replace roboshop123 with your actual password)
+# These are the lines 5 and 6 that failed earlier
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
