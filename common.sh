4dc790d6384b1d7a226f94a480130c53ce48bfed
Gopichand173 <gopi110531@gmail.com>
2026-03-10 04:49:14 -0500

    roboshop code

--- common.sh
+++ common.sh
@@ -2,17 +2,17 @@ NODEJS(){
   dnf module disable nodejs -y
   dnf module enable nodejs:20 -y
   dnf install nodejs -y
-  cp $(component).service /etc/systemd/system/$(component).service
+  cp ${component}.service /etc/systemd/system/${component}.service
 
   useradd roboshop
   mkdir /app
-  curl -o /tmp/$(component).zip https://roboshop-artifacts.s3.amazonaws.com/$(component)-v3.zip
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
   cd /app || exit
-  unzip /tmp/$(component).zip
+  unzip /tmp/${component}.zip
   cd /app || exit
   npm install
 
   systemctl daemon-reload
-  systemctl enable $(component)
-  systemctl start $(component)
+  systemctl enable ${component}
+  systemctl start ${component}
 }
