0af4755ef0624d7b622cc417207a01e8fb46a0c9
Gopichand173 <gopi110531@gmail.com>
2026-03-10 03:24:26 -0500

    roboshop code

--- shipping.sh
+++ shipping.sh
@@ -8,7 +8,7 @@ unzip /tmp/shipping.zip
 cd /app || exit
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
-
+dnf install mysql -y
 mysql -h mysql-dev.devops173.com -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h mysql-dev.devops173.com -uroot -pRoboShop@1 < /app/db/app-user.sql
 mysql -h mysql-dev.devops173.com -uroot -pRoboShop@1 < /app/db/master-data.sql
