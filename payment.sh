fd2edb6314ff822b67f0f1667750ddc8368d1fd8
Gopichand173 <gopi110531@gmail.com>
2026-03-10 04:02:37 -0500

    roboshop code

--- payment.sh
+++ payment.sh
@@ -1,6 +1,7 @@
 dnf install python3 gcc python3-devel -y
 cd payment.service /etc/systemd/system/payment.service || exit
 useradd roboshop
+rm -rf/app
 mkdir /app
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
 cd /app || exit
