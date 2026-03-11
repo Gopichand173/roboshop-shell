2ad951c58f62396e8d2d77a415c5f17f4e259c5b
Gopichand173 <gopi110531@gmail.com>
2026-03-11 12:07:56 -0500

    roboshop code

--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,13 @@
+if [ -z $1 ]; then
+  echo Input Password is missing
+  exit
+  fi
+
 source common.sh
 component=shipping
 MAVEN
 dnf install mysql -y
+##sudo bash shipping.sh Roboshop@1
 mysql -h mysql-dev.devops173.com -uroot -p$1 < /app/db/schema.sql
 mysql -h mysql-dev.devops173.com -uroot -p$1 < /app/db/app-user.sql
 mysql -h mysql-dev.devops173.com -uroot -p$1 < /app/db/master-data.sql
