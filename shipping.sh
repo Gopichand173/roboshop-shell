b3c67125019ae294316c4bbd8b3f1f61ac014bc5
Gopichand173 <gopi110531@gmail.com>
2026-03-11 12:35:43 -0500

    roboshop code

--- shipping.sh
+++ shipping.sh
@@ -1,12 +1,13 @@
 if [ -z $1 ]; then
   echo Input Password is missing
-  exit
+  exit 1
   fi
 
 source common.sh
 component=shipping
 MAVEN
 dnf install mysql -y
+
 ##sudo bash shipping.sh Roboshop@1
 for file in schema app-user master-data; do
 mysql -h mysql-dev.devops173.com -uroot -p$1 < /app/db/$file.sql
