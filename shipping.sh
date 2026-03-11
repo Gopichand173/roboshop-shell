abc9107f06c4d0a527d86b3d0bc5c187a74061ea
Gopichand173 <gopi110531@gmail.com>
2026-03-11 12:22:17 -0500

    roboshop code

--- shipping.sh
+++ shipping.sh
@@ -8,6 +8,6 @@ component=shipping
 MAVEN
 dnf install mysql -y
 ##sudo bash shipping.sh Roboshop@1
-mysql -h mysql-dev.devops173.com -uroot -p$1 < /app/db/schema.sql
-mysql -h mysql-dev.devops173.com -uroot -p$1 < /app/db/app-user.sql
-mysql -h mysql-dev.devops173.com -uroot -p$1 < /app/db/master-data.sql
+for file in schema app-user master-data; do
+mysql -h mysql-dev.devops173.com -uroot -p$1 < /app/db/$file.sql
+done
